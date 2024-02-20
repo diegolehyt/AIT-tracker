360a8cadadc2b7b97708234ebd4d3c57427a10c3
Diego Lehyt <diegolehy00@gmail.com>
2024-02-20 18:07:31 -0500

    tracker input field added

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
 // Initialization for ES Users
-import { Dropdown, initMDB } from "mdb-ui-kit";
+// import { Dropdown, initMDB } from "mdb-ui-kit";
 
-initMDB({ Dropdown });
+// initMDB({ Dropdown });
 
 
 // Initialization for ES Users
 import { Input, Ripple, initMDB } from "mdb-ui-kit";
 
 initMDB({ Input, Ripple });
+
+
+
+$(document).ready(function(){
+    $('.dropdown-submenu a.test').on("click", function(e){
+        $(this).next('ul').toggle();
+        e.stopPropagation();
+        e.preventDefault();
+    });
+    });
