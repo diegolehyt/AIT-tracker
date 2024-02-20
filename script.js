52766e9c2be44bb4c8bde770179a46ecd48686e0
Diego Lehyt <diegolehy00@gmail.com>
2024-02-20 15:50:25 -0500

    login page and styles added

--- script.js
+++ script.js
@@ -2,3 +2,9 @@
 import { Dropdown, initMDB } from "mdb-ui-kit";
 
 initMDB({ Dropdown });
+
+
+// Initialization for ES Users
+import { Input, Ripple, initMDB } from "mdb-ui-kit";
+
+initMDB({ Input, Ripple });
